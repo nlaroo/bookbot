b3f961995e19c1858d55638b4a9c0f92cd550dbe
nlaroo <nicklaroo@gmail.com>
2024-06-20 10:46:06 -0500

    added count words and chracter functions

---
+++ main.py
@@ -0,0 +1,29 @@
+def main():
+    frankenstein_path = "../bookbot/books/frankenstein.txt"
+    text = read_book(frankenstein_path)
+    words = num_of_words(text)
+    letter = num_of_letters(text)
+    print(f"{words} words found in the document")
+
+def num_of_words(text):
+    words = text.split()
+    word_count = len(words)
+    return word_count
+
+
+def read_book(text):
+    with open(text) as f:
+        return f.read()
+    
+def num_of_letters(letter):
+    letter_dict = {}
+    my_string = letter
+    lowered_string = my_string.lower()
+    for l in lowered_string:
+        letter_dict[l] = letter_dict.get(l, 0) + 1
+    print(letter_dict)
+        
+        
+
+main()
+
