268995e80fe0e7113aaf3954c92e0f19ac87f96b
nlaroo <nicklaroo@gmail.com>
2024-06-20 20:02:01 -0500

    added final report

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ def main():
     text = read_book(frankenstein_path)
     words = num_of_words(text)
     letter = num_of_letters(text)
+
+    letter_list = [{'letter': key, 'count': value} for key, value in letter.items()]
+    letter_list.sort(key=lambda x: x["count"], reverse=True)
+
+    print(f"--- Begin report of {frankenstein_path} ---")
     print(f"{words} words found in the document")
-    print(letter)
+    for item in letter_list:
+        print(f"The '{item['letter']}' character was found {item['count']} times")
+    
+    print("--- End report ---")
 
 def num_of_words(text):
     words = text.split()
@@ -24,7 +32,7 @@ def num_of_letters(letter):
         if l.isalpha():
             letter_dict[l] = letter_dict.get(l, 0) + 1
     return letter_dict
-        
+
 
 main()
 
